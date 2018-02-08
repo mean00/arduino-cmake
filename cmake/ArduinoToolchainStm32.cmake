7d57e375b6024b1574da4fe726d714a1866b212a
mean <fixounet@free.fr>
2018-02-08 07:59:35 +0100

    [Arduino-cmake] Begin adding multiple architecture support, in particular stm32. Some stuff are hardcoded for the moment

--- cmake/ArduinoToolchainStm32.cmake
+++ cmake/ArduinoToolchainStm32.cmake
@@ -6,17 +6,27 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 #=============================================================================#
-
-set(CMAKE_C_COMPILER avr-gcc)
-set(CMAKE_ASM_COMPILER avr-gcc)
-set(CMAKE_CXX_COMPILER avr-g++)
+MESSAGE(STATUS "Setting up STM32 arduino cmake environment")
+#
+list(APPEND CMAKE_SYSTEM_PREFIX_PATH "/opt/st32gcc/bin") # FIXME
+SET(USER_INSTALL_FOLDER "/home/fx/.arduino15/packages/arduino") # FIXME
+#
+MACRO(STM32_ADD_SEARCH_PATH subfolder)
+    list(APPEND CMAKE_SYSTEM_PREFIX_PATH ${ARDUINO_SDK_PATH}/hardware/${subfolder})
+    list(APPEND CMAKE_SYSTEM_PREFIX_PATH ${USER_INSTALL_FOLDER}/${subfolder})
+ENDMACRO(STM32_ADD_SEARCH_PATH subfolder)
+#
+SET(STM32_TOOLCHAIN_PREFIX arm-none-eabi-)
+set(CMAKE_C_COMPILER   ${STM32_TOOLCHAIN_PREFIX}gcc)
+set(CMAKE_ASM_COMPILER ${STM32_TOOLCHAIN_PREFIX}gcc)
+set(CMAKE_CXX_COMPILER ${STM32_TOOLCHAIN_PREFIX}g++)
 
 if (ARDUINO_SDK_PATH)
     set(ENV{_ARDUINO_CMAKE_WORKAROUND_ARDUINO_SDK_PATH} "${ARDUINO_SDK_PATH}")
 
-    list(APPEND CMAKE_SYSTEM_PREFIX_PATH ${ARDUINO_SDK_PATH}/hardware/tools/arm-none-eabi-gcc)
-    list(APPEND CMAKE_SYSTEM_PREFIX_PATH ${ARDUINO_SDK_PATH}/hardware/tools/openocd)
-    list(APPEND CMAKE_SYSTEM_PREFIX_PATH ${ARDUINO_SDK_PATH}/hardware/tools/arduinoSTM32load)
+    STM32_ADD_SEARCH_PATH(tools/arm-none-eabi-gcc)
+    STM32_ADD_SEARCH_PATH(tools/openocd)
+    STM32_ADD_SEARCH_PATH(tools/arduinoSTM32load)
 else ()
     message(FATAL_ERROR "Could not find Arduino SDK (set ARDUINO_SDK_PATH)!")
 endif ()
