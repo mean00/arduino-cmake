09a6e1b08495c801e56764b09fff804d110b52cb
mean <fixounet@free.fr>
2018-02-09 08:23:06 +0100

    [arduino-cmake] Per platform linker flags too

--- cmake/Platform/Core/BoardFlags/LinkerFlagsSetter.cmake
+++ cmake/Platform/Core/BoardFlags/LinkerFlagsSetter.cmake
@@ -1,8 +1,1 @@
-# ToDo: Comment
-function(set_board_linker_flags LINKER_FLAGS BOARD_ID IS_MANUAL)
-
-    _get_board_property(${BOARD_ID} build.mcu MCU)
-    set(LINK_FLAGS "-mmcu=${MCU}")
-    set(${LINKER_FLAGS} "${LINK_FLAGS}" PARENT_SCOPE)
-
-endfunction()
+include(${CMAKE_CURRENT_LIST_DIR}/LinkerFlagsSetter${PLATFORM_ARCHITECTURE_POSTFIX}.cmake)
