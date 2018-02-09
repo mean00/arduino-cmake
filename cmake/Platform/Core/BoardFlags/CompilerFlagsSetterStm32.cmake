f8f9072cfc317d27a90ffa7781a8fb98b7cafb3a
mean <fixounet@free.fr>
2018-02-09 08:23:23 +0100

    [arduino-cmake] Stm32 compiler flags (incomplete)

--- cmake/Platform/Core/BoardFlags/CompilerFlagsSetterStm32.cmake
+++ cmake/Platform/Core/BoardFlags/CompilerFlagsSetterStm32.cmake
@@ -1,4 +1,5 @@
-#    _get_board_property(${BOARD_ID} build.f_cpu FCPU)
+#_get_board_property(${BOARD_ID} speed_72mhz.build.f_cpu FCPU) # Sane default...
+SET(FPCU "72000000L")
 #    _get_board_property(${BOARD_ID} build.mcu MCU)
-#    set(COMPILE_FLAGS "-DF_CPU=${FCPU} -DARDUINO=${NORMALIZED_SDK_VERSION} -mmcu=${MCU}")
+set(COMPILE_FLAGS "-DF_CPU=${FCPU} -DARDUINO=${NORMALIZED_SDK_VERSION} ")
 
