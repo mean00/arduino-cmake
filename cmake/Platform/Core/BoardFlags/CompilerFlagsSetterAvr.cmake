9f93044f3a710bbf1bfbe09f24a4f00bbe1e0ae4
mean <fixounet@free.fr>
2018-02-09 07:54:50 +0100

    [arduino-cmake] Per architecture name lookup

---
+++ cmake/Platform/Core/BoardFlags/CompilerFlagsSetterAvr.cmake
@@ -0,0 +1,4 @@
+    _get_board_property(${BOARD_ID} build.f_cpu FCPU)
+    _get_board_property(${BOARD_ID} build.mcu MCU)
+    set(COMPILE_FLAGS "-DF_CPU=${FCPU} -DARDUINO=${NORMALIZED_SDK_VERSION} -mmcu=${MCU}")
+
