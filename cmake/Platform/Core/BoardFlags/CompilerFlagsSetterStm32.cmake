9add9062e28aa5881b41e2fee917db95103d003b
mean <fixounet@free.fr>
2018-02-10 10:01:13 +0100

    [arduino-cmake] Rebuilding the include list (hardcoded at the moment)

--- cmake/Platform/Core/BoardFlags/CompilerFlagsSetterStm32.cmake
+++ cmake/Platform/Core/BoardFlags/CompilerFlagsSetterStm32.cmake
@@ -2,6 +2,16 @@
 SET(FCPU "72000000L") # Hack
 #    _get_board_property(${BOARD_ID} build.mcu MCU)
 set(COMPILE_FLAGS "-DF_CPU=${FCPU} -DARDUINO=${NORMALIZED_SDK_VERSION} ")
-set(COMPILE_FLAGS "${COMPILE_FLAGS} -I\"${${BOARD_CORE}.path}/../../system/libmaple/include\" ") # Hack, there is a better way to get the system path
-set(COMPILE_FLAGS "${COMPILE_FLAGS} -I\"${${BOARD_CORE}.path}/../../system/libmaple/\" ") # Hack, there is a better way to get the system path
+# This should be derived from the arduino config files
+# hardcode them for the moment
+SET(STM32_SYSTEM_ROOT "-I\"${${BOARD_CORE}.path}/../../system/")
+set(COMPILE_FLAGS "${COMPILE_FLAGS} ${STM32_SYSTEM_ROOT}/libmaple/include\" ") # Hack, there is a better way to get the system path
+set(COMPILE_FLAGS "${COMPILE_FLAGS} ${STM32_SYSTEM_ROOT}/libmaple/include/libmaple\" ") # Hack, there is a better way to get the system path
+set(COMPILE_FLAGS "${COMPILE_FLAGS} ${STM32_SYSTEM_ROOT}/libmaple/\" ") # Hack, there is a better way to get the system path
+set(COMPILE_FLAGS "${COMPILE_FLAGS} ${STM32_SYSTEM_ROOT}/libmaple/usb/usb_lib/\" ") # Hack, there is a better way to get the system path
+set(COMPILE_FLAGS "${COMPILE_FLAGS} ${STM32_SYSTEM_ROOT}/libmaple/usb/stm32f1/\" ") # Hack, there is a better way to get the system path
+
+#
+#set(COMPILE_FLAGS "${COMPILE_FLAGS} -std=gnu11 -MMD -DDEBUG_LEVEL=DEBUG_NONE ")
+
 
