fbaca8c8fccfeccf51efc6785db6419301eb3819
mean <fixounet@free.fr>
2018-02-10 09:39:50 +0100

    [arduino-cmake] Add system include folders too, stm32 specific it seems

--- cmake/Platform/Core/BoardFlags/CompilerFlagsSetterStm32.cmake
+++ cmake/Platform/Core/BoardFlags/CompilerFlagsSetterStm32.cmake
@@ -1,5 +1,7 @@
 #_get_board_property(${BOARD_ID} speed_72mhz.build.f_cpu FCPU) # Sane default...
-SET(FPCU "72000000L")
+SET(FCPU "72000000L") # Hack
 #    _get_board_property(${BOARD_ID} build.mcu MCU)
 set(COMPILE_FLAGS "-DF_CPU=${FCPU} -DARDUINO=${NORMALIZED_SDK_VERSION} ")
+set(COMPILE_FLAGS "${COMPILE_FLAGS} -I\"${${BOARD_CORE}.path}/../../system/libmaple/include\" ") # Hack, there is a better way to get the system path
+set(COMPILE_FLAGS "${COMPILE_FLAGS} -I\"${${BOARD_CORE}.path}/../../system/libmaple/\" ") # Hack, there is a better way to get the system path
 
