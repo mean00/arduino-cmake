c55574eccd59c631e1987fb63c376b011e80dbac
mean <fixounet@free.fr>
2018-02-11 10:09:24 +0100

    [arduino-cmake] About to generate elf and hex file

--- cmake/Platform/Core/BoardFlags/LinkerFlagsSetterStm32.cmake
+++ cmake/Platform/Core/BoardFlags/LinkerFlagsSetterStm32.cmake
@@ -3,6 +3,9 @@ function(set_board_linker_flags LINKER_FLAGS BOARD_ID IS_MANUAL)
 
 #    _get_board_property(${BOARD_ID} build.mcu MCU)
     #set(LINK_FLAGS "-mmcu=${MCU}")
+    #set(LINK_FLAGS "${LINK_FLAGS} -Wl,--cref") 
+    set(LINK_FLAGS "${LINK_FLAGS} -Wl,--check-sections -Wl,--gc-sections ")
+    set(LINK_FLAGS "${LINK_FLAGS} -Wl,--unresolved-symbols=report-all -Wl,--warn-common -Wl,--warn-section-align -Wl,--warn-unresolved-symbols -lstdc++")
     set(${LINKER_FLAGS} "${LINK_FLAGS}" PARENT_SCOPE)
 
 endfunction()
