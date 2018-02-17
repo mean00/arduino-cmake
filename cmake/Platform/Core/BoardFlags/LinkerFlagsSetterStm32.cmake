0447890cec4e110fb1e94f5be1c1115b6c51bddd
mean <fixounet@free.fr>
2018-02-17 19:45:22 +0100

    [arduino-cmake] Tweak linker options to avoid issue with float

--- cmake/Platform/Core/BoardFlags/LinkerFlagsSetterStm32.cmake
+++ cmake/Platform/Core/BoardFlags/LinkerFlagsSetterStm32.cmake
@@ -1,10 +1,7 @@
 # ToDo: Comment
 function(set_board_linker_flags LINKER_FLAGS BOARD_ID IS_MANUAL)
 
-#    _get_board_property(${BOARD_ID} build.mcu MCU)
-    #set(LINK_FLAGS "-mmcu=${MCU}")
-    #set(LINK_FLAGS "${LINK_FLAGS} -Wl,--cref") 
-    set(LINK_FLAGS "${LINK_FLAGS} -Wl,--check-sections -Wl,--gc-sections ")
+    set(LINK_FLAGS "${LINK_FLAGS} -Os  -Wl,--gc-sections -mcpu=cortex-m3")
     set(LINK_FLAGS "${LINK_FLAGS} -Wl,--unresolved-symbols=report-all -Wl,--warn-common -Wl,--warn-section-align -Wl,--warn-unresolved-symbols -lstdc++")
     set(${LINKER_FLAGS} "${LINK_FLAGS}" PARENT_SCOPE)
 
