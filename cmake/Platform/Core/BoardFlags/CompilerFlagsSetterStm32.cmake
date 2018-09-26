42927549b99611928880aa5e19f21741e3a1ac6d
mean <fixounet@free.fr>
2018-09-26 21:05:42 +0200

    [simplify]

--- cmake/Platform/Core/BoardFlags/CompilerFlagsSetterStm32.cmake
+++ cmake/Platform/Core/BoardFlags/CompilerFlagsSetterStm32.cmake
@@ -11,13 +11,66 @@ ENDIF(TRY_CPU_FLAGS)
 set(COMPILE_FLAGS "-DF_CPU=${FCPU} ${CPU_FLAGS} -DARDUINO=${NORMALIZED_SDK_VERSION} ")
 # This should be derived from the arduino config files
 # hardcode them for the moment
+
+
+#
+MACRO(DBG)
+    MESSAGE(STATUS ${ARGN})
+ENDMACRO(DBG)
+
+
+MACRO(ADD_IF_DEFINED  key )
+    DBG("Checking ${key}")
+    if( DEFINED ${BOARD_ID}.${key})
+        DBG("Yes ${key}: ${${BOARD_ID}.${key}}")
+        #SET(COMPILE_FLAGS  "${COMPILE_FLAGS} ${${BOARD_ID}.${key}}")
+    endif( DEFINED ${BOARD_ID}.${key})
+ENDMACRO(ADD_IF_DEFINED  key)
+
+MACRO(SET_IF_DEFINED  key out )
+    DBG("Checking ${key}")
+    if( DEFINED ${BOARD_ID}.${key})
+        DBG("Yes  ${key}=>${${BOARD_ID}.${key}} ")
+        #SET(${out}  "${${BOARD_ID}.${key}} ")
+    endif( DEFINED ${BOARD_ID}.${key})
+ENDMACRO(SET_IF_DEFINED  key out)
+
+
+MACRO(ADD_TO_COMPILE_FLAGS key prefix)
+   DBG("Checking ${key}")
+    if( DEFINED ${BOARD_ID}.${key})
+        DBG("Yes ${key} -D${prefix}${${BOARD_ID}.${key}} ")
+        #SET(COMPILE_FLAGS "${COMPILE_FLAGS} -D${prefix}${${BOARD_ID}.${key}} ")
+    endif( DEFINED ${BOARD_ID}.${key})
+ENDMACRO(ADD_TO_COMPILE_FLAGS key prefix)
+#
 SET(STM32_SYSTEM_ROOT "-I\"${${BOARD_CORE}.path}/../../system/")
-set(COMPILE_FLAGS "${COMPILE_FLAGS} ${STM32_SYSTEM_ROOT}/libmaple/include\" ") # Hack, there is a better way to get the system path
-set(COMPILE_FLAGS "${COMPILE_FLAGS} ${STM32_SYSTEM_ROOT}/libmaple/include/libmaple\" ") # Hack, there is a better way to get the system path
 set(COMPILE_FLAGS "${COMPILE_FLAGS} ${STM32_SYSTEM_ROOT}/libmaple/\" ") # Hack, there is a better way to get the system path
+set(COMPILE_FLAGS "${COMPILE_FLAGS} ${STM32_SYSTEM_ROOT}/libmaple/include\" ") # Hack, there is a better way to get the system path
 set(COMPILE_FLAGS "${COMPILE_FLAGS} ${STM32_SYSTEM_ROOT}/libmaple/usb/usb_lib/\" ") # Hack, there is a better way to get the system path
 set(COMPILE_FLAGS "${COMPILE_FLAGS} ${STM32_SYSTEM_ROOT}/libmaple/usb/stm32f1/\" ") # Hack, there is a better way to get the system path
 
+
+# ZZ SET_IF_DEFINED(build.vect vec1)
+# ZZ SET_IF_DEFINED(menu.cpu.${ARDUINO_UPLOAD_METHOD}Method.build.vect vec2)
+
+IF(DEFINED vec2)
+    # ZZ SET(COMPILE_FLAGS "${COMPILE_FLAGS} -D${vec2}")
+ELSE(DEFINED vec2)
+    # ZZ SET(COMPILE_FLAGS "${COMPILE_FLAGS} -D${vec1}")
+ENDIF(DEFINED vec2)
+
+# upload flags if any
+#ZZ ADD_IF_DEFINED(menu.cpu.${ARDUINO_UPLOAD_METHOD}Method.build.upload_flags )
+#ZZ ADD_IF_DEFINED(menu.cpu.${ARDUINO_CPU}.build.cpu_flags)
+
+ADD_TO_COMPILE_FLAGS(build.error_led_port  "ERROR_LED_PORT=")
+ADD_TO_COMPILE_FLAGS(build.error_led_pin  "ERROR_LED_PIN=")
+ADD_TO_COMPILE_FLAGS(build.board  "ARDUINO_")
+ADD_TO_COMPILE_FLAGS(build.variant  "BOARD_")
+
+
+DBG("Compile flags = ${COMPILE_FLAGS}")
 #
 #set(COMPILE_FLAGS "${COMPILE_FLAGS} -std=gnu11 -MMD -DDEBUG_LEVEL=DEBUG_NONE ")
 
--- cmake/Platform/Initialization/DefaultCFlagsStm32.cmake
+++ cmake/Platform/Initialization/DefaultCFlagsStm32.cmake
@@ -1,4 +1,4 @@
 # -fno-rtti 
 # -fno-exceptions
 # -x c++
-SET(ARDUINO_DEFAULT_CFLAGS " -ffunction-sections -fdata-sections -nostdlib --param max-inline-insns-single=500 -DBOARD_generic_stm32f103r8 -DVECT_TAB_ADDR=0x8002000 -DERROR_LED_PORT=GPIOB -DERROR_LED_PIN=1 -w   -mcpu=cortex-m3 -DF_CPU=72000000L -DARDUINO=10801 -DARDUINO_GENERIC_STM32F103R -DARDUINO_ARCH_STM32F1  -DSERIAL_USB -DGENERIC_BOOTLOADER -DMCU_STM32F103R8 -mthumb  -fno-rtti  -fno-exceptions  -DCONFIG_MAPLE_MINI_NO_DISABLE_DEBUG   -g ")
+SET(ARDUINO_DEFAULT_CFLAGS " -ffunction-sections -fdata-sections -nostdlib --param max-inline-insns-single=500  -DVECT_TAB_ADDR=0x8002000 -w   -mcpu=cortex-m3 -DF_CPU=72000000L -DARDUINO=10801  -DARDUINO_ARCH_STM32F1  -DSERIAL_USB -DGENERIC_BOOTLOADER -DMCU_STM32F103R8 -mthumb  -fno-rtti  -fno-exceptions  -DCONFIG_MAPLE_MINI_NO_DISABLE_DEBUG   -g ")
