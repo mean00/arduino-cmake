70943afbf2f0a29b11500057c20bb38b0800c2df
mean <fixounet@free.fr>
2018-09-26 21:09:58 +0200

    [simplify]

--- cmake/Platform/Initialization/DefaultCFlagsStm32.cmake
+++ cmake/Platform/Initialization/DefaultCFlagsStm32.cmake
@@ -1,4 +1,5 @@
 # -fno-rtti 
 # -fno-exceptions
 # -x c++
-SET(ARDUINO_DEFAULT_CFLAGS " -ffunction-sections -fdata-sections -nostdlib --param max-inline-insns-single=500  -DVECT_TAB_ADDR=0x8002000 -w   -mcpu=cortex-m3 -DF_CPU=72000000L -DARDUINO=10801  -DARDUINO_ARCH_STM32F1  -DSERIAL_USB -DGENERIC_BOOTLOADER -DMCU_STM32F103R8 -mthumb  -fno-rtti  -fno-exceptions  -DCONFIG_MAPLE_MINI_NO_DISABLE_DEBUG   -g ")
+SET(ARDUINO_DEFAULT_CFLAGS " -ffunction-sections -fdata-sections -nostdlib --param max-inline-insns-single=500  -w   -mcpu=cortex-m3  -mthumb  -fno-rtti  -fno-exceptions  -g ")
+SET(ARDUINO_DEFAULT_CFLAGS " ${ARDUINO_DEFAULT_CFLAGS} -DVECT_TAB_ADDR=0x8002000  -DF_CPU=72000000L -DARDUINO=10801  -DARDUINO_ARCH_STM32F1  -DSERIAL_USB -DGENERIC_BOOTLOADER -DMCU_STM32F103R8 -DCONFIG_MAPLE_MINI_NO_DISABLE_DEBUG    ")
