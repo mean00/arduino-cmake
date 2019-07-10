d1063f66344b6d20e9ac232475145ac1e276f4d9
mean <fixounet@free.fr>
2019-07-10 07:52:06 +0200

    [stm32] Add option to use SET(ARDUINO_USE_NEWLIB 1) to use nano newlib and have smaller executables

--- Platform/Core/BoardFlags/LinkerFlagsSetterStm32.cmake
+++ Platform/Core/BoardFlags/LinkerFlagsSetterStm32.cmake
@@ -3,6 +3,10 @@ function(set_board_linker_flags LINKER_FLAGS BOARD_ID IS_MANUAL)
 
     set(LINK_FLAGS "${LINK_FLAGS} -Os  -Wl,--gc-sections -mcpu=cortex-m3")
     set(LINK_FLAGS "${LINK_FLAGS} -Wl,--unresolved-symbols=report-all -Wl,--warn-common -Wl,--warn-section-align -lstdc++")
+IF(ARDUINO_USE_NEWLIB) # smaller
+    set(LINK_FLAGS "${LINK_FLAGS} --specs=nano.specs")
+ENDIF(ARDUINO_USE_NEWLIB) # smaller
+
 #-Wl,--warn-unresolved-symbols -lstdc++")
     set(${LINKER_FLAGS} "${LINK_FLAGS}" PARENT_SCOPE)
 
